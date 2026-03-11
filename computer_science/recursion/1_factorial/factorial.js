990aba0d8af86e7f925dc18b17011c064d3f8d61
yko-git <yko.iwata@gmail.com>
2026-03-12 06:40:15 +0900

    1_factorial/1_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,7 @@
-const factorial = function() {
-
+const factorial = function (num) {
+  if (!Number.isInteger(num) || num < 0) return;
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
