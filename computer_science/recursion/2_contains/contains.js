0a6b5f12aef2925fe720c830450b0d30a0888e17
yko-git <yko.iwata@gmail.com>
2026-03-13 09:45:06 +0900

    2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,11 @@
-const contains = function() {
-  
+const contains = function (object, target) {
+  for (let obj of Object.values(object.data)) {
+    if (Object.values(obj).includes(target)) {
+      return true;
+    }
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
