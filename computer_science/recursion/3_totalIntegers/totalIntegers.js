e8ad1a0b5e26eb04dd6ff95e972562ebee6f8ef8
yko-git <yko.iwata@gmail.com>
2026-03-14 10:36:49 +0900

    3_totalintegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,14 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (value, count = 0) {
+  for (let obj of value) {
+    if (typeof obj === "number") {
+      count++;
+    }
+    if (typeof obj === "object") {
+      count += totalIntegers(obj);
+    }
+  }
+  return count;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
