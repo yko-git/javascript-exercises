202dfdeee24776de85ea33a6422a12b913c1f572
yko-git <yko.iwata@gmail.com>
2026-03-15 11:53:03 +0900

    4_permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,17 @@
-const permutations = function() {
-  
+const permutations = function (array) {
+  if (array.length === 0) return [[]];
+  if (array.length === 1) return [array];
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let current = array[i];
+    let remainning = array.filter((x) => x !== current);
+    let subPermutations = permutations(remainning);
+    for (let perm of subPermutations) {
+      result.push([current, ...perm]);
+    }
+  }
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
